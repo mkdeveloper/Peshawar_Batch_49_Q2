333f05d82632e3910252d2ae819e622a4e71ca0a
mkdeveloper <khubaib.mk@gmail.com>
2023-11-17 11:42:51 +0500

    last week classwork

---
+++ week_01/class-01/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  const name = "Khubaib";
+  return (
+    <>
+      <h1 className={"header"}>{name}</h1>
+      <p style={{ background: "red", color: "white" }}>He is learning GenAi</p>
+    </>
+  );
+};
+
+export default Home;
